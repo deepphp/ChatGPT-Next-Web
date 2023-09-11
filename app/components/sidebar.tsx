9abd311b714a710d746277039d56657b2600f28a
deepphp <129658186+deepphp@users.noreply.github.com>
2023-09-11 18:31:37 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           Alex
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          永无止境的探索，停留下的终究是｛｝
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+            <a href={https://chatgpt.yunweb.cf/} target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
